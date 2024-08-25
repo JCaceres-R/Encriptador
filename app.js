58aba3e3ffcb8839fc09541178e1ff1426bd6396
JCaceres-R <scaceresr70@gmail.com>
2024-08-25 15:28:26 -0500

    Final
    se cumplen los requisitos del trello

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+document.getElementById('textoingreso').addEventListener('input', function (e) {
+  const invalidChars = /[^a-z\s]/g;
+
+  if (invalidChars.test(e.target.value)) {
+      alert("Por favor, solo escribe letras minúsculas y espacios.");
+      e.target.value = e.target.value.replace(invalidChars, '');
+  }
+});
+
+
 function accionEncriptar() {
 
-    const Texto = document.getElementById("Texto").value;
+    const Texto = document.getElementById("textoingreso").value;
     let Arreglo = Texto.split("");
 
     Arreglo.forEach( (letra,index) => {
@@ -30,10 +40,15 @@ function accionEncriptar() {
     });
     console.log(Arreglo)
     document.getElementById("resultado").innerHTML = Arreglo.join("");
+    console.log(Arreglo.join(""))
   }
 
+
+
+
+  
 function accionDesencriptar() {
-  const Texto = document.getElementById("Texto").value;
+  const Texto = document.getElementById("textoingreso").value;
   
   let banco = {
     "ai":"a",
@@ -45,6 +60,31 @@ function accionDesencriptar() {
 
   let TextoDesencriptado = Texto.replace(/(ai|enter|imes|ober|ufat)/g, function(i) {return banco[i]});
   document.getElementById("resultado").innerHTML = TextoDesencriptado;
+
 }
 
 
+
+
+function copiarContenido() {
+  let texto = document.getElementById('resultado').innerHTML;
+    navigator.clipboard.writeText(texto);
+}
+
+function pegarContenido() {
+  navigator.clipboard.readText().then(texto => {
+      document.getElementById("textoingreso").value = texto;
+  }).catch(err => {
+      console.error('Error al leer el portapapeles: ', err);
+  });
+}
+
+
+function borrar(tex){
+  console.log(tex.value);
+  document.getElementById(tex).innerHTML="";
+  if (tex == "textoingreso"){
+    document.getElementById(tex).value="";
+  }
+  console.log(tex.value);
+}
